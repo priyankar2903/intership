8b9e6a39272b0f8be0a9708378c4e8fdcd0f81ce
priyankar2903 <priyankarane482@gmail.com>
2025-06-26 15:18:47 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,76 @@
+#if else
+'''age=int(input("enter age:"))
+if age>=18:
+    print("user is adult")
+else:
+     print("user is not adult")'''
+
+#percentage
+'''per=float(input("enter percentage"))
+if per>=40:
+    print("student is pass")
+else:
+    print("student is fail")'''    
+
+    #even odd
+'''num=int(input("enter the number"))
+if num%2==0:
+    print("even number")
+else:
+    print("odd number")''' 
+
+    #leap -not leap  
+'''year=int(input("enter the year"))
+if year%4==0:
+    print("leap year")
+else:
+    print("not leap year") ''' 
+
+    #max number
+'''num1=int(input("enter the number"))
+num2=int(input("enter the number"))
+if num1>num2:
+    print(num1,"is max")
+else:
+    print(num2,"is max") ''' 
+
+   #positive,negative
+'''num=int(input("enter number"))
+if num>0:
+    print("number is positive")
+elif num==0:
+    print("number is netural")
+else:
+    print("number is negative")''' 
+
+#grade of student 
+'''per=int(input("enter the percentage"))
+if per>=80:
+    print("A grade")
+elif per>=60:
+    print("B grade")
+elif per>40:
+    print("C grade")
+else:
+    print("fail")'''                
+
+#greater three number
+'''num1=int(input("enter number"))
+num2=int(input("enter number"))
+num3=int(input("enter number"))
+if num1>num2 and num1>num3:
+    print(num1,"is max")
+elif num2>num1 and num2>num3:
+    print(num2,"is max") 
+else:
+    print(num3,"is max")'''   
+
+ #if else nest 
+'''num1=int(input("enter number"))
+num2=int(input("enter number"))
+num3=int(input("enter number"))
+if num1>num2:
+    if num1>num3:
+        print(num1,"is max")
+    else:
+        print(num2,"is max") '''       
