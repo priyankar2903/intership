0d7cb0e09b90f279fcaa85e76c4b6d481d621c76
priyankar2903 <priyankarane482@gmail.com>
2025-06-26 15:19:20 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,30 @@
+num1=int(input('enter any number : '))
+num2=int(input('enter any number : '))
+res=num1+num2
+print('Result : ',res)
+
+print(type(num1))
+print(type(num2))
+
+str=input('enter any alphabet ')
+print(type(str))
+
+r=int(input('enter radius of circle :'))
+py=3.14
+print('Area of circle :',py*(r**2))
+
+side=int(input('enter side of square : '))
+print('Area of square : ',side**2)
+
+dollars=int(input('enter dollars : '))
+rs=dollars*85
+print("dollars converted into rupees are : ",rs)
+
+rup=int(input('enter ruppees : '))
+dlr=rup/85
+print('Ruppees converted into dollars are :',dlr)
+
+s1=int(input('enter side of triangle :'))
+s2=int(input('enter side of triangle :'))
+s3=int(input('enter side of triangle :'))
+print('Area of triangle :',s1+s2+s3)
